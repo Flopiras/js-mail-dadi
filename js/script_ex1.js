43aa6d88439393990939709ac850db19062d3a58
Flopiras <Flopiras@yahoo.it>
2023-05-31 17:18:26 +0200

    add comment and result

--- js/script_ex1.js
+++ js/script_ex1.js
@@ -8,23 +8,24 @@ emailList.push('darth.vader@obiwan.com' , 'mylittlepony@fairyland.com' , 'super.
 //FARE VERIFICA
 console.log(emailList)
 
-let emailItem = ('');
-
 //2 CHIEDERE ALL'UTENTE LA PROPRIA MAIL
 const userEmail = prompt('Inserisci qui la tua email', 'darth.vader@obiwan.com').trim();
 
-let result = '';
+//Creare una costante collegata alla mail dell'utente (fuori dal ciclo)
+console.log(userEmail);
 
-//3  CREARE UN CICLO DI CONTROLLO PER GLI ELEMENTI DELL'ARRAY
+//3. Creare una variabile di "saluto"
+let result = 'Accesso negato';
+
+//4  CREARE UN CICLO DI CONTROLLO PER GLI ELEMENTI DELL'ARRAY
 for(let i = 0; i<emailList.length; i++){
-    emailItem = emailList[i];
-    
-    if(userEmail != emailItem){
-        result = 'Accesso negato!';
-    }
-    else if{ 
+     let emailItem = emailList[i];    
+     
+     //SE la mail è già presente nel ciclo, mandiamo un messaggio di bevenuto
+     if(userEmail == emailItem){
         result = 'Benvenuto!';
     }
 }
-    
+
+//6. Stampare il messaggio
 console.log(result)
